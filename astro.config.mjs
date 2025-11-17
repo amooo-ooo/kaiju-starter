d9280c458cdb6ffbf51e1f647e49fb816a4a84f9
Amor Budiyanto <amor.budiyanto@gmail.com>
2025-11-17 21:15:22 +1300

    feat: simplify page (temp)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  integrations: [
+    sitemap(),
+    UnoCSS()
+  ]
 });
